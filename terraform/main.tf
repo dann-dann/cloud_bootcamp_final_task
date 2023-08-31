b36e23ba952bf7abddded34b66f66ddbc29e23d7
dann-dann <danila1176@gmail.com>
2023-08-31 16:20:47 +0300

    Add dynamic get latest ubuntu AMI ID

--- terraform/main.tf
+++ terraform/main.tf
@@ -79,9 +79,27 @@ resource "aws_security_group" "daniils-final-sg" {
 
 }
 
+# Get latest ubuntu AMI id
+# https://www.andreagrandi.it/2017/08/25/getting-latest-ubuntu-ami-with-terraform/
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"] # Canonical AWS account ID for Ubuntu AMIs
+}
+
 # Creating EC2 instance
 resource "aws_instance" "daniils-final-instance" {
-  ami           = "ami-04e601abe3e1a910f" # Canonical, Ubuntu, 22.04 LTS, amd64 jammy image build on 2023-05-16
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name      = "daniils-key" 
 
