ddd3132898fe7fbdd7a9dd2369c2950ee48db94a
dann-dann <danila1176@gmail.com>
2023-08-31 17:25:59 +0300

    Add outbound rule SSH

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,13 +37,21 @@ resource "aws_security_group" "daniils-final-sg" {
   }
 
   #############################
-  # Outbound rule [All TCP] 
+  # Outbound rule [All TCP]
   egress {
      from_port = 0
      to_port = 65535
      protocol = "tcp"
      cidr_blocks = ["0.0.0.0/0"]
    }
+
+   # Outbound rule [SSH]
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
 }
 
