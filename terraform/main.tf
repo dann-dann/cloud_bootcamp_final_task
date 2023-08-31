739def071c718d9fe769e631ca304f3d4e562add
dann-dann <danila1176@gmail.com>
2023-08-31 16:30:27 +0300

    Remove unnecessary inbound/outbound rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,39 +36,7 @@ resource "aws_security_group" "daniils-final-sg" {
     cidr_blocks = ["0.0.0.0/0"]  
   }
 
-  # Inbound rule for [HTTPS]
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   #############################
-  # Outbound rule [SSH]
-  egress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Outbound rule [HTTP]
-  egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Outbound rule [HTTPS]
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   # Outbound rule [All TCP] 
   egress {
      from_port = 0
